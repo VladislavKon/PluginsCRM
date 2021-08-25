3da2294fe80fdcf6431d874e32085b91d8f4abc1
Владислав Коньшин <vkonshin@myTrialCRMorg.onmicrosoft.com>
2021-08-25 18:13:59 +0500

    Сделал автоподстановку региона

--- Webresources/webresources/regionAuto.js
+++ Webresources/webresources/regionAuto.js
@@ -1,12 +1,33 @@
 ﻿function regionAuto() {
-    try {
-        if (Xrm.Page.getControl("new_city") != null && Xrm.Page.getControl("new_city") != undefined) {
-            var City = Xrm.Page.getAttribute("new_city").getValue();            
-            Xrm.Page.getAttribute("new_region").setValue();
+    try {        
+        if (Xrm.Page.getAttribute("new_city").getValue() != null && Xrm.Page.getAttribute("new_city").getValue() != undefined) {
+            var City = Xrm.Page.getAttribute("new_city").getValue();
+            var EntityCity = "new_mycity";
+            var SelectCity = "?$select=new_cityname,_new_regionlookup_value";
+            var FilterCity = "&$filter=new_cityname eq '" + City[0].name + "'";
+            Xrm.WebApi.retrieveMultipleRecords(EntityCity, SelectCity + FilterCity).then(
+                function success(result) {
+                    if (result != null) {
+                        GetRegion(result.entities[0])
+                    }
+                }                
+            );            
         }
         else {
             return;
         }
+    } catch (e) {
+        throw new Error(e.Message);
+    }
+}
+function GetRegion(CityResult) {
+    try {
+        var regionLookup = [];
+        regionLookup[0] = new Object();
+        regionLookup[0].id = CityResult["_new_regionlookup_value"];
+        regionLookup[0].name = CityResult["_new_regionlookup_value@OData.Community.Display.V1.FormattedValue"];
+        regionLookup[0].entityType = "new_region";
+        Xrm.Page.getAttribute("new_region").setValue(regionLookup);
     } catch (e) {
         throw new Error(e.Message);
     }
