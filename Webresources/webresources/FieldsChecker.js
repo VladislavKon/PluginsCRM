1725e08550b391bef84291bbbc2448ad53156104
Vladislav Konshin <vladislav.konshin@inbox.ru>
2021-08-27 12:32:21 +0500

    Исправления

--- Webresources/webresources/FieldsChecker.js
+++ Webresources/webresources/FieldsChecker.js
@@ -1,16 +1,16 @@
 ﻿function FieldsChecker(primaryControl) {
     var message = "Введите данные в поля: "
-    if (Xrm.Page.getAttribute("new_city").getValue() != null && Xrm.Page.getAttribute("new_city").getValue() != undefined
-        && Xrm.Page.getAttribute("new_region").getValue() != null && Xrm.Page.getAttribute("new_region").getValue() != undefined
-        && Xrm.Page.getAttribute("new_email").getValue() != null && Xrm.Page.getAttribute("new_email").getValue() != undefined) {
+    if (primaryControl.getAttribute("new_city").getValue() != null && primaryControl.getAttribute("new_city").getValue() != undefined
+        && primaryControl.getAttribute("new_region").getValue() != null && primaryControl.getAttribute("new_region").getValue() != undefined
+        && primaryControl.getAttribute("new_email").getValue() != null && primaryControl.getAttribute("new_email").getValue() != undefined) {
         primaryControl.data.save()
     } else {
-        if (Xrm.Page.getAttribute("new_city").getValue() != null && Xrm.Page.getAttribute("new_city").getValue() != undefined)
-            message += "\n Город"
-        if (Xrm.Page.getAttribute("new_region").getValue() != null && Xrm.Page.getAttribute("new_city").getValue() != undefined)
-            message += "\n Регион"
-        if (Xrm.Page.getAttribute("new_email").getValue() != null && Xrm.Page.getAttribute("new_city").getValue() != undefined)
-            message += "\n Основной email"
+        if (primaryControl.getAttribute("new_city").getValue() == null || primaryControl.getAttribute("new_city").getValue() == undefined)
+            message += "\n Город";
+        if (primaryControl.getAttribute("new_region").getValue() == null || primaryControl.getAttribute("new_region").getValue() == undefined)
+            message += "\n Регион";
+        if (primaryControl.getAttribute("new_email").getValue() == null || primaryControl.getAttribute("new_email").getValue() == undefined)
+            message += "\n Основной email";
         Xrm.Navigation.openAlertDialog(message);        
     }
 }
